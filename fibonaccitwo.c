c956ac0b0e3a889af90477238a1fd975136d9c10
Simone Mendoza <simone.mendoza@live.com>
2012-07-06 21:36:07 -0700

    Converted fibonacci code from Java to C

---
+++ fibonaccitwo.c
@@ -0,0 +1,37 @@
+/*
+  author:Simone Mendoza
+  date:July 6, 2012
+  This program will give an n number of Fibonacci numbers.
+*/
+#include<stdio.h>
+
+main()
+{
+      int n;
+ 
+      printf("ENTER THE NUMBER OF FIBONACCI YOU WANT.\n");
+      scanf("%d", &n);
+      printf("THESE ARE THE FIBONACCI NUMBERS:\n");
+  
+      int f;
+      int i;     
+
+      for ( i = 0 ; i <= n ; i++ )
+      {
+         f = fib(i);
+         printf("%d\n", f);
+      }
+      return 0;
+
+}
+
+   /*
+     Computes a Fibonacci number.
+   */
+int fib(int n)
+{
+      if (n < 2)
+           return n;
+      else
+           return fib(n - 1) + fib(n - 2);
+}
